b98d03e0aaa917e0fbd85da32485ffdd1382e5a8
Han Wang <hanwangx@gmail.com>
2022-06-06 12:25:07 +0100

    chore: more on redux and redux thunk

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,26 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Header,
   Footer,
-  SideMenu,
   Carousel,
+  SideMenu,
   ProductCollection,
   BusinessPartners,
 } from "../../components";
-import { Row, Col, Typography } from "antd";
-import styles from "./HomePage.module.css";
-import { productList1, productList2, productList3 } from "./mockups";
+import { Row, Col, Typography, Spin } from "antd";
 import sideImage from "../../assets/images/sider_2019_12-09.png";
 import sideImage2 from "../../assets/images/sider_2019_02-04.png";
 import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
+import styles from "./HomePage.module.css";
+import { withTranslation, WithTranslation } from "react-i18next";
+import { connect } from "react-redux";
+import { RootState } from "../../redux/store";
+import { giveMeDataActionCreator } from "../../redux/recommendProducts/recommendProductsActions";
 
+const mapStateToProps = (state: RootState) => {
+  return {
+    loading: state.recommendProducts.loading,
+    error: state.recommendProducts.error,
+    productList: state.recommendProducts.productList
+  }
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    giveMeData: () => {
+      dispatch(giveMeDataActionCreator());
+    }
+  };
+};
 
-export class HomePage extends Component {
+type PropsType = WithTranslation &
+  ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
+
+class HomePageComponent extends React.Component<PropsType> {
+  componentDidMount() {
+    this.props.giveMeData();
+  }
+
   render() {
+    // console.log(this.props.t)
+    const { t, productList, loading, error } = this.props;
+    if (loading) {
+      return (
+        <Spin
+          size="large"
+          style={{
+            marginTop: 200,
+            marginBottom: 200,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "100%",
+          }}
+        />
+      );
+    }
+    if (error) {
+      return <div>网站出错：{error}</div>;
+    }
     return (
-      <div>
+      <>
         <Header />
+        {/* 页面内容 content */}
         <div className={styles["page-content"]}>
           <Row style={{ marginTop: 20 }}>
             <Col span={6}>
@@ -30,38 +75,42 @@ export class HomePage extends Component {
               <Carousel />
             </Col>
           </Row>
-
           <ProductCollection
             title={
               <Typography.Title level={3} type="warning">
-                爆款推荐
+                {t("home_page.hot_recommended")}
               </Typography.Title>
             }
             sideImage={sideImage}
-            products={productList1}
+            products={productList[0].touristRoutes}
           />
           <ProductCollection
             title={
               <Typography.Title level={3} type="danger">
-                新品上市
+                {t("home_page.new_arrival")}
               </Typography.Title>
             }
             sideImage={sideImage2}
-            products={productList2}
+            products={productList[1].touristRoutes}
           />
           <ProductCollection
             title={
               <Typography.Title level={3} type="success">
-                国内游推荐
+                {t("home_page.domestic_travel")}
               </Typography.Title>
             }
             sideImage={sideImage3}
-            products={productList3}
+            products={productList[2].touristRoutes}
           />
           <BusinessPartners />
         </div>
         <Footer />
-      </div>
+      </>
     );
   }
 }
+
+export const HomePage = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withTranslation()(HomePageComponent));
---
+++ src/redux/hooks.ts
@@ -0,0 +1,6 @@
+import {useSelector as useReduxSelector, TypedUseSelectorHook} from "react-redux"
+
+
+import {RootState} from "./store"
+
+export const useSelector: TypedUseSelectorHook <RootState> =useReduxSelector;
